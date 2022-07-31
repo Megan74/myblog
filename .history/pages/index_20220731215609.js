17d925560548ac5289520f25ad3dc5178867bc37
“Megan <mmegaan@gmail.com>
2022-07-31 22:05:40 +0200

    --all 

---
+++ .history/pages/index_20220731215609.js
@@ -0,0 +1,70 @@
+import Link from 'next/link'
+import Head from 'next/head'
+import { allPosts } from 'contentlayer/generated'
+import { pick } from '@contentlayer/client'
+import utilStyles from '../styles/utils.module.css'
+
+export default function Home({ posts }) {
+  return (
+    <>
+      <Head>
+        <title>Home</title>
+      </Head>
+      <style jsx global>{`
+        @import url("https://fonts.googleapis.com/css2?family=Nunito:wght@400;700&display=swap|Roboto:wght@400;700&display=swap");
+        html,
+        body {
+          margin: 0;
+          padding: 0;
+          font-family: Roboto, -apple-system, BlinkMacSystemFont, Segoe UI,
+            Roboto, Oxygen, Ubuntu, Cantarell, Open Sans, Helvetica Neue,
+            sans-serif;
+          background: #333;
+          color: #fff;
+          font-size: 1rem;
+        }
+        h1,
+        h2,
+        h3,
+        h4,
+        h5,
+        h6 {
+          font-weight: 700;
+          font-family: Nunito, -apple-system, BlinkMacSystemFont, Segoe UI,
+            Roboto, Oxygen, Ubuntu, Cantarell, Open Sans, Helvetica Neue,
+            sans-serif;
+        }
+        a {
+          color: #fff;
+        }
+        .content {
+          max-width: 600px;
+          margin: 2rem auto;
+          padding: 0 1rem;
+        }
+     
+
+     
+        <h2 className={utilStyles.headingLg}>Blog</h2>
+        <ul className={utilStyles.list}>
+          {posts.map(({ slug, date, title }) => (
+            <li className={utilStyles.listItem} key={slug}>
+              <Link href={`/posts/${slug}`}>
+                <a>{title}</a>
+              </Link>
+              <br />
+              <small className={utilStyles.lightText}>{date}</small>
+            </li>
+          ))}
+        </ul>
+     
+      `}</style>
+    </>
+  )
+}
+
+export async function getStaticProps() {
+  const posts = allPosts.map((post) => pick(post, ['title', 'date', 'slug']))
+
+  return { props: { posts } }
+}
---
+++ .history/pages/index_20220731220333.js
@@ -0,0 +1,35 @@
+import Link from 'next/link'
+import Head from 'next/head'
+import { allPosts } from 'contentlayer/generated'
+import { pick } from '@contentlayer/client'
+import Layout from '../components/Layout'
+
+export default function Home({ posts }) {
+  return (
+    <>
+      <Head>
+        <title>Home</title>
+      </Head>
+
+        <Layout>
+        <h2 className={utilStyles.headingLg}>Blog</h2>
+        <ul className={utilStyles.list}>
+          {posts.map(({ slug, date, title }) => (
+            <li className={utilStyles.listItem} key={slug}>
+              <Link href={`/posts/${slug}`}>
+                <a>{title}</a>
+              </Link>
+              <br />
+              <small>{date}</small>
+            </li>
+          ))}
+        </ul>
+    </>
+  )
+}
+
+export async function getStaticProps() {
+  const posts = allPosts.map((post) => pick(post, ['title', 'date', 'slug']))
+
+  return { props: { posts } }
+}
